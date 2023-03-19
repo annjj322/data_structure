ab1e76688df6296d549990411d091bf06ee0583d
Junje <87896157+annjj322@users.noreply.github.com>
2023-03-19 19:35:07 +0900

    CBA3 Final
    Final version of CBA3

--- CBA3.py
+++ CBA3.py
@@ -50,3 +50,55 @@ if __name__ == '__main__':
 
     result_function = Poly3.add(third_order,forth_order)
     result_function.linked_list.pprint()
+
+
+
+
+##### Poly1, Poly2 #####
+class Poly1:
+    def __init__(self, maxOrder): # append를 사용하지 x. 크기를 미리 정해뒀기 때문에 append를 사용하는 방식이 아님
+        self.maxOrder = maxOrder
+        self.coef = [0]*(self.maxOrder+1)
+
+    def setCoef(self, coef, order):
+        self.coef[order] = coef # order 자리에 coef를 넣어라
+
+    def getCoef(self, order):
+        if order > self.maxOrder:
+            return 0
+        else:
+            return self.coef[order]
+
+    @classmethod
+    def add(cls, p, q):
+        maxOrder = max(p.maxOrder, q.maxOrder)
+        r = Poly1(maxOrder)
+        for i in range(maxOrder+1):
+            rCoef = p.getCoef(i) + q.getCoef(i)
+            r.setCoef(rCoef,i)
+        return r
+    
+
+class Poly2:
+    def __init__(self,maxOrder):
+        self.maxOrder = maxOrder
+        self.coef = []
+    def setCoef(self, coef):
+        self.coef.append(coef)
+
+    def getCoef(self, order):
+        if order > self.maxOrder:
+            return 0
+        else:
+            for i in range(len(self.coef)):
+                if self.coef[i][1] == order:
+                    return self.coef[i][0]
+            return 0 # for loop를 다 돌아도 order 상에서 None으로 정의된 항들이 있기 때문에 0을 return 해주는 것.
+
+    @classmethod
+    def add(cls,p,q):
+        maxOrder = max(p.maxOrder, q.maxOrder)
+        r = Poly2(maxOrder)
+        for i in range(maxOrder+1):
+            r.setCoef((p.getCoef(i)+q.getCoef(i),i)) # setting self
+        return r
